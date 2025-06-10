9ccd2acaa859ce47029b6b37860e168e3069b4f3
Kevin Adrian <kevin19305.ib@gmail.com>
2025-06-10 11:15:42 +0700

    menambhakan private: dibagian class queues

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class queues {
+private:
     int FRONT, REAR, max = 5;      // Variabel penanda depan dan belakang queue serta ukuran maksimal
     int queue_array[5];           // Array untuk menyimpan elemen queue
 
