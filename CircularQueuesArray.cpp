7294fe6165ce5aa32e7e7c7d3fce63da1b4b6582
Kevin Adrian <kevin19305.ib@gmail.com>
2025-06-10 10:40:08 +0700

    Array untuk menyimpan elemen queue

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -3,4 +3,4 @@ using namespace std;
 
 class queues {
     int FRONT, REAR, max = 5;      // Variabel penanda depan dan belakang queue serta ukuran maksimal
-    
+    int queue_array[5];           // Array untuk menyimpan elemen queue
