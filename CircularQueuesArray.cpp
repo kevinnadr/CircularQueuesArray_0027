c39f426c16ad68e4169a39d7e61928fd027c9ca8
Kevin Adrian <kevin19305.ib@gmail.com>
2025-06-10 10:41:01 +0700

    inisialisasi jika queue kosong

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -4,3 +4,9 @@ using namespace std;
 class queues {
     int FRONT, REAR, max = 5;      // Variabel penanda depan dan belakang queue serta ukuran maksimal
     int queue_array[5];           // Array untuk menyimpan elemen queue
+
+public:
+    queues() {                    // Konstruktor: inisialisasi queue kosong
+        FRONT = -1;
+        REAR = -1;
+    }
