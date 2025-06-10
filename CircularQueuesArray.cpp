8cd5b215027325837e3bcedfc70a74f3058fa8d7
Kevin Adrian <kevin19305.ib@gmail.com>
2025-06-10 10:43:52 +0700

    untuk Cek apakah queue penuh (circular condition)

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -16,3 +16,9 @@ public:
         cout << "enter a number: ";
         cin >> num;
         cout << endl;
+
+        // Cek apakah queue penuh (circular condition)
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nqueue overflow\n";
+            return;
+        }
