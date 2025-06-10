389280dfd6c4b7896e4b5e51d699475d8082973a
Kevin Adrian <kevin19305.ib@gmail.com>
2025-06-10 11:02:49 +0700

    digunakan untuk menampilkan seluruh elemen queue jika queue dalam keadaan circular, yaitu ketika FRONT lebih besar dari REAR. Karena data queue "terputus" di array (sebagian di akhir, sebagian di awal), maka:

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -81,4 +81,19 @@ public:
             }
             cout << endl;
         }
- 
+        else {
+            // Jika circular (FRONT > REAR), tampilkan dua bagian
+            while (FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+    }
+};
