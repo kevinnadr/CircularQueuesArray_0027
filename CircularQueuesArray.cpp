bce30dc9b7d2fcce0fa2d96a6ac1117eea8924c4
Kevin Adrian <kevin19305.ib@gmail.com>
2025-06-10 10:39:35 +0700

    Variabel penanda depan dan belakang queue serta ukuran maksimal

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -1,2 +1,6 @@
 #include <iostream>                // Library untuk input dan output
 using namespace std;
+
+class queues {
+    int FRONT, REAR, max = 5;      // Variabel penanda depan dan belakang queue serta ukuran maksimal
+    
